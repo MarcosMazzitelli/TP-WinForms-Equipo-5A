ac05110ba0dec17850c43d8305d905adecfd0189
MarcosMazzitelli <marcosmazzitelli89@gmail.com>
2025-09-09 20:27:07 -0300

    Creación capa negocio
    Creación de capa negocio junto con la clase Acceso a datos con los atributos conexión, lector y comando. Junto con los métodos que permiten realizar una conexión a la base de datos de forma simplificada

---
+++ TPWinForm_equipo-5A/negocio/AccesoDatos.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace negocio
+{
+    public class AccesoDatos
+    {
+        private SqlConnection conexion;
+        private SqlCommand comando;
+        private SqlDataReader lector;
+
+        public SqlDataReader Lector
+        {
+            get {  return lector; }
+
+        }
+
+
+        public AccesoDatos()
+        {                                 //NombreServidor(el punto es genérico) ; NombreBaseDeDatos ; Forma de concetarse(Windows authentication)
+            conexion = new SqlConnection("server=.\\SQLEXPRESS; database=CATALOGO_P3_DBM; integrated security=true");
+            comando = new SqlCommand();
+        }
+
+        public void setearConsulta(string consulta)
+        {
+            //comando realiza la acción e inyecta la consulta SQL de tipo texto (recibido por parámetro).
+            comando.CommandType = System.Data.CommandType.Text;
+            comando.CommandText = consulta;
+        }
+
+        public void ejecutarLectura()
+        {
+            //ejecuta el comando (consulta SQL) en la conexion (BD)
+            comando.Connection = conexion;
+            try
+            {
+                //se abre la conexion y se realiza la lectura.
+                conexion.Open();
+                lector = comando.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void cerrarConexion()
+        {
+            //En caso de haber un lector activo cuando se llama al método, también se cierra junto con la conexión.
+            if (lector != null)
+                lector.Close();
+            conexion.Close();
+        }
+    }
+}
